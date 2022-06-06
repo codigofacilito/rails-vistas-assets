de12f56b3490691abd4549076548c7848e956485
Uriel Hernández <uriel.hdzc@gmail.com>
2022-06-06 11:36:25 -0500

    Activestorage wip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :body)
+      params.require(:post).permit(:title, :body, :cover)
     end
 end
