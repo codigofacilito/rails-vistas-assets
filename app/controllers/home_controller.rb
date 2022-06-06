25c38728e79e550bbb347b9097af617a0b4d1e38
Uriel Hernández <uriel.hdzc@gmail.com>
2022-06-06 15:31:18 -0500

    Layouts

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  layout "landing"
+  def index
+  end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
+  layout :set_layout
 
   # GET /posts or /posts.json
   def index
@@ -66,5 +67,9 @@ class PostsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def post_params
       params.require(:post).permit(:title, :body, :cover)
+    end
+
+    def set_layout
+      %w(show).include?(action_name) ? "landing" : "application"
     end
 end
