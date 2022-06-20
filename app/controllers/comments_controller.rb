55b7cc3c0ad5c5b9c72df03b0b439f0dc1da296f
Uriel Hernández <uriel.hdzc@gmail.com>
2022-06-19 23:01:57 -0500

    Comments wit turbo streams

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,15 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     @comment.user_id = current_user.id
     @comment.save
-    redirect_to post_path(@comment.post_id)
+    
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to post_path(@comment.post_id) }
+        format.turbo_stream 
+      else
+        format.html { redirect_to post_path(@comment.post_id), notice: "No pudimos añadir el comentario" }
+      end
+    end
   end
 
   private
